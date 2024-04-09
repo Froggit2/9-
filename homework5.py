7a36222cf34c9b7dad29bbae8356037cfad37af7
Froggit2 <165292357+Froggit2@users.noreply.github.com>
2024-04-09 06:51:39 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,11 @@
+my_list = ['apple', 'orange', 'banana', 'mango', 'avocado', 'blueberry', 'cherry']
+print('Список: ',my_list)
+print('Первое значение: ',my_list[0])
+print('Последнее значение: ',my_list[-1])
+print('Значения с 3 по 5 включая 3 и 5: ',my_list[3:6])
+my_dict = {'Key1': 135791113151719, 'Key2': 24681012141618, 'Orange':'оранжевый'}
+print('Словарь: ',my_dict)
+print('Первый ключ в словаре: ',my_dict['Key1'])
+my_dict['Key1'] = 174324478
+my_dict['Key3'] = 124436
+print('Замена ключа и добавление нового: ',my_dict)
